c717d27ef14824faff55ee718ef72f475d1bdcde
Ab-verse <abhisheksckm17@gmail.com>
2022-11-27 18:17:24 +0530

    Contract deployed on goerli network

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const OceanToken = await hre.ethers.getContractFactory("OceanToken");
-  const oceanToken = await OceanToken.deploy(100000000, 50);
+  const AbToken = await hre.ethers.getContractFactory("AbToken");
+  const abToken = await AbToken.deploy(100000000, 50);
 
-  await oceanToken.deployed();
+  await abToken.deployed();
 
-  console.log("Ocean Token deployed: ", oceanToken.address);
+  console.log("Ocean Token deployed: ", abToken.address);
 }
 
 main().catch((error) => {
